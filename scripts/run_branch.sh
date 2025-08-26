9525b20af529df78286cab257ad505492362f1a7
Pete Pedersen <pete.pedersen@geant.org>
2025-08-26 13:18:31 +0000

    small changes to that only two release pipeline run in paralelle

--- scripts/run_branch.sh
+++ scripts/run_branch.sh
@@ -13,14 +13,14 @@ branch=$1
 check="\<${branch}\>"
 if [[ ${BRANCH_LIST[@]} =~ ${check} ]]
 then
-  echo "##starting branch : ${branch}"
+  echo -e "##starting branch : ${branch}\n"
   PACKAGE_LIST=( `cat ${CONF_DIR}/${branch}.list` )
-  echo -e "* list of distributions: ${OS_LISTi[*]}"
+  echo -e "* list of distributions: ${OS_LIST[*]}\n"
   # loop to do the processing of all the containers
   # ------------------------------------------------
   for OS in ${OS_LIST[*]}
   do
-    echo -e "### ${OS}"
+    echo -e "### ${OS}\n"
     LOG_DIR="${WWW_REPORTS}/$TODAY/${branch}/${OS}"
     mkdir -p ${LOG_DIR}
     os_version=${OS/*-/}
@@ -33,11 +33,11 @@ then
     echo -ne "* Creating base image ${IMAGE_ALIAS}"
     bash ${BASE_DIR}/create_template.sh ${OS/-*/} ${os_version} ${branch} &>> /dev/null
     if [ $? -ne 0 ]; then
-      echo -e "\t\tFAILED more info in /tmp/create_template.sh.out"
+      echo -e "\t\tFAILED more info in /tmp/create_template.sh.out\n"
        #exit 10
        continue # skip this one do next OS
     else
-      echo -e "\t\tDONE"
+      echo -e "\t\tDONE\n"
     fi
     package_loop_start="`date -u '+%Y/%m/%d %H:%M'`"
     #echo "    starting ${OS}"
@@ -46,9 +46,10 @@ then
     for PACKAGE in ${PACKAGE_LIST[*]}
     do
       #echo -e "\t`date -u '+%Y/%m/%d %H:%M'` autopkgtest ${PACKAGE} using image ${IMAGE_ALIAS}"
-      echo -e "\n####${PACKAGE} using image ${IMAGE_ALIAS}"
+      echo -e "####${PACKAGE} using image ${IMAGE_ALIAS}\n"
       autopkgtest -d -U --summary-file=${LOG_DIR}/autopkgtest_${PACKAGE}_summary.txt ${PACKAGE} -- lxd local:${IMAGE_ALIAS} &>> ${LOG_DIR}/autopkgtest_${PACKAGE}_debug.log
       sed -e 's/^/            /' ${LOG_DIR}/autopkgtest_${PACKAGE}_summary.txt | grep -v PASS
+      echo
     done # for all package list
     echo 
   done # for all OS list
@@ -62,3 +63,4 @@ else
   exit 100
 fi
 
+markdown2 -x toc /var/www/html/reports/screen_output_${branch}_$(date +'%Y-%m-%d').log > /var/www/html/reports/screen_output_${branch}_$(date +'%Y-%m-%d').html
--- scripts/start_multi_job_all.sh
+++ scripts/start_multi_job_all.sh
@@ -1,19 +1,42 @@
 #!/usr/bin/bash
 # This scrip only function is to spawn a detched screen session for each branch  
+# it now limit the number of concurrent jobs
+# you can see the progress with:
+#   ps -u $USER 
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"
 BASE_DIR=$(dirname $0)
 CONF_DIR=${BASE_DIR}/../conf
 BRANCH_LIST=( `grep -v '^#' ${CONF_DIR}/repo.list` )
 WWW_REPORTS=/var/www/html/reports
+MAX_JOBS=2
+WAIT_SLEEP=60
+JOB_TOTAL=${#BRANCH_LIST[*]}
+JOB_COUNT=0
+MYNAME=`cat /proc/$$/comm`
+
+# this function will count the number of screen session 
+# and will wait for the count to be bellow ${MAX_JOB}
+# before returning
+function wait_for_job(){
+    # get scree session cound
+    SCREEN_COUNT=`screen -list |awk -v 'c=0'  '{if($2 ~ /Socket/){if($1 == "No"){c=0}else{c=$1}}};END{print c}'`
+    while [ ${SCREEN_COUNT} -ge ${MAX_JOBS} ]
+    do
+        sleep ${WAIT_SLEEP}
+        SCREEN_COUNT=`screen -list |awk -v 'c=0'  '{if($2 ~ /Socket/){if($1 == "No"){c=0}else{c=$1}}};END{print c}'`
+    done
+}
 
 # loop for looping over the different release branch
 # --------------------------------------------------
 for branch in ${BRANCH_LIST[*]}
 do
+  wait_for_job
   # spawn screen detached session for current branch of loop 
   echo starting ${branch} screen session ...
   screen -t ${branch} -L -Logfile /var/www/html/reports/screen_output_${branch}_$(date +'%Y-%m-%d').log -d -m bash ${BASE_DIR}/run_branch.sh ${branch}
+  echo "${MYNAME} progress: $JOB_COUNT/${JOB_TOTAL}" > /proc/$$/comm
 done # for all branchs
 #exit 1
 
